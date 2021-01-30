e24b56008b38fee7965250f4c9f8b40ae2e5c23c
Khalifa Mamadou NIAMADIO <khalifaniamadio@gmail.com>
2021-01-30 16:06:36 +0000

    ajout de l'arriere plan du jeu

--- main.py
+++ main.py
@@ -5,12 +5,23 @@ pygame.init()
 # generer le titre de la fenetre
 
 pygame.display.set_caption("big bang")
-pygame.display.set_mode((1080, 700))
+screen = pygame.display.set_mode((1080, 700))
+
+# importer l'arriere plan du jeu
+background = pygame.image.load('assets/bg.jpg')
+
 
 running = True
 # tant que cette condition est vraie maintenir la fenetre  ouverte
 
 while running:
+
+    # appliquer l'arriere plan du jeu
+    screen.blit(background, (0, -200))
+
+    # mettre a jour l'ecran
+    pygame.display.flip()
+
     # si le joueur ferme cette fenetre
     for event in pygame.event.get():
 
